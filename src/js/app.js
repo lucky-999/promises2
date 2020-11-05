84c4aacfb39ccb5e352475382163787aaeec1e32
Ilya Tatarinov <itatarinov7@gmail.com>
2020-11-05 12:02:21 +0300

    HW done

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,10 @@
-// TODO: write your code here
-import sum from './basic';
+import GameSavingLoader from './GameSavingLoader';
 
-console.log('worked');
-
-console.log(sum([1, 2]));
+(async () => {
+  try {
+    const saving = await GameSavingLoader.load();
+    console.log(`saving as string = ${JSON.stringify(saving)}`);
+  } catch (error) {
+    console.error(`rejected load: ${error}`);
+  }
+})();
