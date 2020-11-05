ab0b2c6639363f617057de9852e271fb38cc30e8
Ilya Tatarinov <itatarinov7@gmail.com>
2020-11-05 17:45:37 +0300

    HW

---
+++ src/js/GameSavingLoader.js
@@ -0,0 +1,18 @@
+import read from './reader';
+import json from './parser';
+
+export default class GameSavingLoader {
+  static async load() {
+    try {
+      const data = await read();
+      const result = await json(data);
+      try {
+        return JSON.parse(result);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    } catch (error) {
+      return error;
+    }
+  }
+}
